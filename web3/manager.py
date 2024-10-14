3ea63973d545957e7bc2424633c0a4e91b25d206
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-14 04:57:01 +0000

    debugs

--- web3/manager.py
+++ web3/manager.py
@@ -211,7 +211,7 @@ class RequestManager:
         Couroutine for making a request using the provider
         """
         response = await self._coro_make_request(method, params)
-        assert "result" in response or "error" in response, (response, type(response), self.middleware_onion.items())
+        assert "result" in response or "error" in response, (response, type(response), 'middlewares:', *self.middleware_onion.middlewares)
         return self.formatted_response(response,
                                        params,
                                        error_formatters,
