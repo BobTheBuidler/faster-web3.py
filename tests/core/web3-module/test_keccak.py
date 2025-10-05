b15dfcf6e6429eb55a123e7f39851726c746e8f3
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-05 06:39:44 -0400

    Update test_keccak.py

--- tests/core/web3-module/test_keccak.py
+++ tests/core/web3-module/test_keccak.py
@@ -1,8 +1,5 @@
 import pytest
 
-from faster_eth_utils.exceptions import (
-    ValidationError,
-)
 from faster_hexbytes import (
     HexBytes,
 )
@@ -112,11 +109,6 @@ def test_keccak_raise_if_primitive_and(kwargs):
     # must not set more than one input
     with pytest.raises(TypeError):
         Web3.keccak("", **kwargs)
-
-
-def test_keccak_raise_if_hexstr_and_text():
-    with pytest.raises(ValidationError):
-        Web3.keccak(hexstr="0x", text="")
 
 
 def test_keccak_raise_if_no_args():
