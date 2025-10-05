ed93b72427e73788e699d207184bdfa3bb84d16d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-05 04:21:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         "eth-hash[pycryptodome]>=0.5.1",
         "eth-typing>=5.0.0",
         "faster-eth-abi>=5.0.1",
-        "faster-eth-utils>=5.0.0",
+        "faster-eth-utils>=5.3.8",
         "faster-hexbytes>=1.2.0",
         "aiohttp>=3.7.4.post0",
         "pydantic>=2.4.0",
