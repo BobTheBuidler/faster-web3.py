1eeda18225e65822b8152593cb56e3c0578a3a8a
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-10-12 02:39:07 +0000

    feat: use less @curry

--- faster_web3/_utils/blocks.py
+++ faster_web3/_utils/blocks.py
@@ -1,5 +1,6 @@
 from typing import (
     Any,
+    Callable,
     Optional,
 )
 
@@ -10,9 +11,6 @@ from faster_eth_utils import (
     is_string,
     is_text,
     remove_0x_prefix,
-)
-from faster_eth_utils.toolz import (
-    curry,
 )
 
 from faster_web3.exceptions import (
@@ -59,21 +57,22 @@ def is_hex_encoded_block_number(value: Any) -> bool:
     return 0 <= value_as_int < 2**256
 
 
-@curry
 def select_method_for_block_identifier(
-    value: Any, if_hash: RPCEndpoint, if_number: RPCEndpoint, if_predefined: RPCEndpoint
-) -> Optional[RPCEndpoint]:
-    if is_predefined_block_number(value):
-        return if_predefined
-    elif isinstance(value, bytes):
-        return if_hash
-    elif is_hex_encoded_block_hash(value):
-        return if_hash
-    elif is_integer(value) and (0 <= value < 2**256):
-        return if_number
-    elif is_hex_encoded_block_number(value):
-        return if_number
-    else:
-        raise Web3ValueError(
-            f"Value did not match any of the recognized block identifiers: {value}"
-        )
+    if_hash: RPCEndpoint, if_number: RPCEndpoint, if_predefined: RPCEndpoint
+) -> Callable[[Any], Optional[RPCEndpoint]]:
+    def select_method_for_block_identifier_curried(value: Any) -> Optional[RPCEndpoint]:
+        if is_predefined_block_number(value):
+            return if_predefined
+        elif isinstance(value, bytes):
+            return if_hash
+        elif is_hex_encoded_block_hash(value):
+            return if_hash
+        elif is_integer(value) and (0 <= value < 2**256):
+            return if_number
+        elif is_hex_encoded_block_number(value):
+            return if_number
+        else:
+            raise Web3ValueError(
+                f"Value did not match any of the recognized block identifiers: {value}"
+            )
+    return select_method_for_block_identifier_curried
