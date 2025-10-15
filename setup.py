fac4d29da71aa37dc0f6c37ebed2b96e6cd260ca
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-15 05:28:58 +0000

    fix: broken build on Windows

--- setup.py
+++ setup.py
@@ -106,6 +106,10 @@ else:
     web3_data_files = sorted(
         str(p.as_posix()) for p in Path("faster_web3/_utils/contract_sources").rglob("*.py")
     )
+    if sys.platform.startswith('win'):
+        # error C2026: string too big, trailing characters truncated
+        web3_data_files.remove("faster_web3/_utils/contract_sources/contract_data/_custom_contract_data.py")
+        
     ens_data_files = ["faster_ens/abis.py", "faster_ens/contract_data.py"]
 
     flags = [
