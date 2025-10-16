e77743caa273b2cee3b2b95116fe05e11fe891da
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-10-16 09:08:12 +0000

    fix

--- faster_web3/exceptions.py
+++ faster_web3/exceptions.py
@@ -387,6 +387,7 @@ class PersistentConnectionError(Web3Exception):
 
 
 @final
+@mypyc_attr(native_class=False)
 class ReadBufferLimitReached(PersistentConnectionError, Web3ValueError):
     """
     Raised when the read buffer limit is reached while reading data from a persistent
