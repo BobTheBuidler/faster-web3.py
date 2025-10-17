91b6fc27e9ae66bbfb8e6cea1a3c69b24feb10ea
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-10-17 06:32:01 +0000

    fix

--- faster_web3/providers/persistent/request_processor.py
+++ faster_web3/providers/persistent/request_processor.py
@@ -109,13 +109,14 @@ class RequestProcessor:
             Callable[..., Any],
         ],
     ) -> Optional[str]:
+        provider = self._provider
         cached_requests_key = generate_cache_key((method, params))
-        if cached_requests_key in self._provider._request_cache._data:
-            cached_response = self._provider._request_cache._data[cached_requests_key]
+        if cached_requests_key in provider._request_cache._data:
+            cached_response = provider._request_cache._data[cached_requests_key]
             cached_response_id = cached_response.get("id")
             cache_key = generate_cache_key(cached_response_id)
             if cache_key in self._request_information_cache:
-                self._provider.logger.debug(
+                provider.logger.debug(
                     "This is a cached request, not caching request info because it is "
                     "not unique:\n    method=%s,\n    params=%s",
                     method,
@@ -123,7 +124,7 @@ class RequestProcessor:
                 )
                 return None
 
-        if request_id is None and not self._provider._is_batching:
+        if request_id is None and not provider._is_batching:
             raise Web3ValueError(
                 "Request id must be provided when not batching requests."
             )
@@ -134,19 +135,17 @@ class RequestProcessor:
             params,
             response_formatters,
         )
-        self._provider.logger.debug(
+        provider.logger.debug(
             "Caching request info:\n    request_id=%s,\n"
             "    cache_key=%s,\n    request_info=%s",
             request_id,
             cache_key,
             request_info.__dict__,
         )
-        self._request_information_cache.cache(
-            cache_key,
-            request_info,
-        )
-        if self._request_information_cache.is_full():
-            self._provider.logger.warning(
+        request_info_cache = self._request_information_cache
+        request_info_cache.cache(cache_key, request_info)
+        if request_info_cache.is_full():
+            provider.logger.warning(
                 "Request information cache is full. This may result in unexpected "
                 "behavior. Consider increasing the ``request_information_cache_size`` "
                 "on the provider."
@@ -226,7 +225,7 @@ class RequestProcessor:
 
         if response_id is not None:
             cache_key = generate_cache_key(response_id)
-            cached_request_info_for_id: RequestInformation = (
+            cached_request_info_for_id = (
                 self._request_information_cache.get_cache_entry(cache_key)
             )
             if cached_request_info_for_id is not None:
@@ -263,12 +262,14 @@ class RequestProcessor:
     ) -> None:
         if subscription:
             if self._subscription_response_queue.full():
-                self._provider.logger.debug(
+                provider = self._provider
+                provider.logger.debug(
                     "Subscription queue is full. Waiting for provider to consume "
                     "messages before caching."
                 )
-                self._provider._listen_event.clear()
-                await self._provider._listen_event.wait()
+                listen_event = provider._listen_event
+                listen_event.clear()
+                await listen_event.wait()
 
             self._provider.logger.debug(
                 "Caching subscription response:\n    response=%s", raw_response
@@ -310,29 +311,32 @@ class RequestProcessor:
         self, cache_key: Optional[str] = None, subscription: bool = False
     ) -> Any:
         if subscription:
-            qsize = self._subscription_response_queue.qsize()
-            raw_response = await self._subscription_response_queue.get()
+            queue = self._subscription_response_queue
+            qsize = queue.qsize()
+            raw_response = await queue.get()
 
-            if not self._provider._listen_event.is_set():
-                self._provider._listen_event.set()
+            provider = self._provider
+            listen_event = provider._listen_event
+            if not listen_event.is_set():
+                listen_event.set()
 
             if qsize == 0:
                 if not self._subscription_queue_synced_with_ws_stream:
                     self._subscription_queue_synced_with_ws_stream = True
-                    self._provider.logger.info(
+                    provider.logger.info(
                         "Subscription response queue synced with websocket message "
                         "stream."
                     )
             else:
                 if self._subscription_queue_synced_with_ws_stream:
                     self._subscription_queue_synced_with_ws_stream = False
-                self._provider.logger.info(
+                provider.logger.info(
                     "Subscription response queue has %s subscriptions. "
                     "Processing as FIFO.",
                     qsize,
                 )
 
-            self._provider.logger.debug(
+            provider.logger.debug(
                 "Subscription response popped from queue to be processed:\n"
                 "    raw_response=%s",
                 raw_response,
