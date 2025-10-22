d3f5667c2ff0e46cd651e131d11dcec8da061022
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-22 08:12:51 -0400

    feat(benchmark): bench address utils (#167)

---
+++ benchmarks/web3/utils/test_address_benchmarks.py
@@ -0,0 +1,42 @@
+import pytest
+from pytest_codspeed import BenchmarkFixture
+
+import web3.utils.address
+import faster_web3.utils.address
+
+SENDER = "0x" + "00" * 20
+
+NONCES = [0, 100, 10000]
+
+SALT = "0x" + "ab" * 32
+
+INIT_CODES = [
+    "0x6000600055",
+    "0x" + "ff" * 64,
+    "0x" + "aa" * 256,
+]
+INIT_CODE_IDS = ["short", "med", "long"]
+
+create = lambda fn: pytest.mark.benchmark(group="get_create_address")(
+    pytest.mark.parametrize("nonce", NONCES)(fn)
+)
+
+create2 = lambda fn: pytest.mark.benchmark(group="get_create2_address")(
+    pytest.mark.parametrize("init_code", INIT_CODES, ids=INIT_CODE_IDS)(fn)
+)
+
+@create
+def test_get_create_address(benchmark: BenchmarkFixture, nonce):
+    benchmark(web3.utils.address.get_create_address, SENDER, nonce)
+
+@create
+def test_faster_get_create_address(benchmark: BenchmarkFixture, nonce):
+    benchmark(faster_web3.utils.address.get_create_address, SENDER, nonce)
+
+@create2
+def test_get_create2_address(benchmark: BenchmarkFixture, init_code):
+    benchmark(web3.utils.address.get_create2_address, SENDER, SALT, init_code)
+
+@create2
+def test_faster_get_create2_address(benchmark: BenchmarkFixture, init_code):
+    benchmark(faster_web3.utils.address.get_create2_address, SENDER, SALT, init_code)
