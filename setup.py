8b02d35a31947c63bfb94779de13ddf5a819d550
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-26 02:25:23 +0000

    fix(mypy): enable err code truthy-function

--- setup.py
+++ setup.py
@@ -150,7 +150,6 @@ else:
         "--disable-error-code=call-arg",
         "--disable-error-code=dict-item",
         "--disable-error-code=typeddict-item",
-        "--disable-error-code=truthy-function",
         "--disable-error-code=var-annotated",
         "--disable-error-code=assignment",
         "--disable-error-code=index",
