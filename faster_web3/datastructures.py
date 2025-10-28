f9d8e7fa73550fbafa8ea7b90e0962080fceedf7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-28 17:41:06 +0000

    feat: cache `AttributeDict.__hash__` result

--- faster_web3/datastructures.py
+++ faster_web3/datastructures.py
@@ -139,6 +139,8 @@ class AttributeDict(ReadableAttributeDict[TKey, TValue], Hashable):
     Provides superficial immutability, someone could hack around it
     """
 
+    __hash: Optional[int] = None
+
     def __setattr__(self, attr: str, val: TValue) -> None:
         if attr == "__dict__":
             super().__setattr__(attr, val)
@@ -153,7 +155,11 @@ class AttributeDict(ReadableAttributeDict[TKey, TValue], Hashable):
         )
 
     def __hash__(self) -> int:
-        return hash(tuple(sorted(tupleize_lists_nested(self).items())))
+        cached_hash = self.__hash
+        if cached_hash is None:
+            # The hash value was not cached, let's compute it and store it
+            cached_hash = self.__hash = hash(tuple(sorted(tupleize_lists_nested(self).items())))
+        return cached_hash
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, AttributeDict):
