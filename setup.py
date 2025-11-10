2406d46f11e776550acaf30c25d882faef3f8d61
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-11-10 17:17:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -206,7 +206,7 @@ setup(
         "eth-account>=0.13.6",
         "eth-hash[pycryptodome]>=0.5.1",
         "eth-typing>=5.0.0",
-        "faster-eth-abi==5.2.19",
+        "faster-eth-abi==5.2.20",
         "faster-eth-utils==5.3.18",
         "faster-hexbytes>=1.2.0",
         "aiohttp>=3.7.4.post0",
